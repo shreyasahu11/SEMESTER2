fd1583a9ac8348536d9ce99684c9dae2214a5109
Shreya Sahu <sahushreya211@gmail.com>
2026-03-19 13:21:57 +0530

    DOM

--- JS/dom.js
+++ JS/dom.js
@@ -1,25 +1,29 @@
+
+// document.writeln("hello");
+
 function result(){
-    //take user input and display in h2 heading
+// take user input and dispaly in h2 heading
     let inputData=document.getElementById("inputText").value;
-    document.getElementById('text').textContent=inputData;
-
-    //valid age for vote casting 
-    if(inputData>=18){
-        alert("valid Data");
-    }
-    else{
-        alert("Not Valid");
-    }
-
+  document.getElementById('text').textContent=inputData;
+    
+// valid age for vote casting!....
+if(inputData>=18){
+    alert("Valid Data");
+}
+else{
+    alert("not valid");
+}
+//change color of heading on click
     let changeColor=document.getElementById('text');
-    changeColor.style.color="brown";
+    changeColor.style.color="red";
+
 
     let bodyColor=document.getElementsByTagName('body')[0];
-    bodyColor.style.backgroundColor="beige";
+    bodyColor.style.backgroundColor="blue";
 }
 
 let para=document.getElementById("paratext");
 function changetextColor(){
-    para.style.backgroundColor="#"+ Math.floor(Math.random() * 167772)
+    para.style.backgroundColor="#"+Math.floor(Math.random() * 16777215).toString(16);
 }
-para.addEventListener('mouseover', changetextColor);
+para.addEventListener('mouseover',changetextColor);
